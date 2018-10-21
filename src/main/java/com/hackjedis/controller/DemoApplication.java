b9b1356deb49b3d7ea4716db536c5cc681c6c980
Rama Narayan Lakshmanan <ramanarayan94@gmail.com>
2018-10-20 21:18:12 -0500

    Fixing project setup

--- src/main/java/com/hackjedis/controller/DemoApplication.java
+++ src/main/java/com/hackjedis/controller/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.hackjedis.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hackjedis/controller/DemoApplicationTests.java
+++ src/test/java/com/hackjedis/controller/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.hackjedis.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
